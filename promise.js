afe526e42f7a2ef25b6245f42b35e56c3f898d43
barkahharis <barkahris@gmail.com>
2022-06-12 15:55:14 +0700

    promise

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async value => {
+  let [dataIXX, dataVGC] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    .then(res => res)
+    .catch(err => console.log(err))
+
+  let result = 0
+  let arrData = []
+  if (Array.isArray(dataIXX) && Array.isArray(dataVGC)) {
+    arrData = [...dataIXX, ...dataVGC]
+    result = arrData.filter(item => item.hasil === value).length
+  }
+  return result
+};
+
 
 module.exports = {
   promiseOutput,
